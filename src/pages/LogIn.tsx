ea8e5f14cb9556fc8cc07037d64879856166f7d7
styuporo <yuliia.porokhniava@student.hs-anhalt.de>
2023-09-10 14:53:19 +0300

    Edited Log In page

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -72,7 +72,7 @@ function LogInForm() {
 
   return (
     <Container
-      className="vh-100 d-flex justify-content-center align-items-center"
+      className="vh-100 justify-content-center align-items-center"
       fluid
       style={{
         backgroundPosition: "center",
@@ -105,8 +105,8 @@ function LogInForm() {
             </Form>
           </Modal.Body>
         </Modal.Dialog>
+        <ToastContainer />
       </div>
-      <ToastContainer />
     </Container>
   );
 }
