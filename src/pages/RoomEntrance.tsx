bfcd2b2eaaeb2552f1497191d18a1041a005e388
styuporo <yuliia.porokhniava@student.hs-anhalt.de>
2023-09-10 14:48:16 +0300

    Edited log out on RoomEntrance

--- src/pages/RoomEntrance.tsx
+++ src/pages/RoomEntrance.tsx
@@ -64,7 +64,7 @@ function RoomEntrance() {
 
   return (
     <Container
-      className="vh-100 d-flex justify-content-center align-items-center"
+      className="vh-100 justify-content-center align-items-center"
       fluid
       style={{
         backgroundPosition: "center",
