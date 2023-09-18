1fd61ab027a10d2b5e4fa666a560903b10347a4e
styuporo <yuliia.porokhniava@student.hs-anhalt.de>
2023-09-18 14:32:34 +0300

    Edited Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,34 +3,36 @@ import Container from "react-bootstrap/Container";
 
 function Footer() {
   return (
-    <Container
-      fluid
-      className="pt-3 text-center"
-      style={{ backgroundColor: "#f9e15d" }}
-    >
-      <Row className="justify-content-md-center">
-        <Col lg="10">
-          <h5>
-            Hochschule Anhalt. Modul Web- und Medienprogrammierung <br />
-            Lehrbeauftragter: Toni Barth
-          </h5>
-          <Row className="justify-content-md-center">
-            <Col md lg="2">
-              <p>Yuliia Porokhniava</p>
-            </Col>
-            <Col md lg="2">
-              <p> Halyna Huzenko</p>
-            </Col>
-            <Col md lg="2">
-              <p>Arina Podoshvelieva</p>
-            </Col>
-            <Col md lg="2">
-              <p>Nataliia Stankevych</p>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </Container>
+    <footer>
+      <Container
+        fluid
+        className="pt-3 text-center"
+        style={{ backgroundColor: "#f9e15d" }}
+      >
+        <Row className="justify-content-md-center">
+          <Col lg="10">
+            <h5>
+              Hochschule Anhalt. Modul Web- und Medienprogrammierung <br />
+              Lehrbeauftragter: Toni Barth
+            </h5>
+            <Row className="justify-content-md-center">
+              <Col md lg="2">
+                <p>Yuliia Porokhniava</p>
+              </Col>
+              <Col md lg="2">
+                <p> Halyna Huzenko</p>
+              </Col>
+              <Col md lg="2">
+                <p>Arina Podoshvelieva</p>
+              </Col>
+              <Col md lg="2">
+                <p>Nataliia Stankevych</p>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
 }
 
