029ade74de036f893e6100130682878c41cc09d4
styuporo <yuliia.porokhniava@student.hs-anhalt.de>
2023-09-28 23:05:45 +0200

    fixed: creating a room with login from the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -47,49 +47,49 @@ function Navigation() {
   };
 
   // Function to handle room creation.
-  const handleCreateRoom = async (e: any) => {
-    e.preventDefault();
-    const username = localStorage.getItem("username");
-    const userID = localStorage.getItem("userID");
+  const handleCreateRoom = () => {
+    const username = localStorage.getItem("username"); // Get the username from local storage.
+    const userID = localStorage.getItem("userID"); // Get the user ID from local storage.
 
-    try {
-      // Send a POST request to create a room.
-      const response = await axios.post(`${server}/rooms/`);
-      if (response.status === 201) {
-        const roomid = response.data.name;
+    if (username === "" || username === null) {
+      // If the user is not logged in, show an error pop-up and navigate to the login page.
+      error_pop_up("You have to log in first!");
+      navigate(`/login`);
+    } else {
+      // If the user is logged in, send a POST request to create a room.
+      axios
+        .post(`${server}/rooms/`)
+        .then((response) => {
+          if (response.status === 201) {
+            const roomid = response.data.name; // Get the room ID from the response.
+            localStorage.setItem("roomid", roomid); // Store the room ID in local storage.
 
-        if (userID === "" || userID === null) {
-          // If the user is not logged in, store the room URL and show an error pop-up.
-          localStorage.setItem("tmpURL", `/room/${roomid}`);
-          error_pop_up("You have to log in first!");
-          navigate("/login");
-        } else {
-          try {
             // Send a PUT request to add the user to the room.
-            const userResponse = await axios.put(
-              `${server}/rooms/${roomid}/users`,
-              { user: userID }
-            );
-            if (userResponse.status === 200) {
-              // Show a success pop-up for successful room creation and entry.
-              success_pop_up("You successfully created and entered the Room");
-              navigate(`/room/${roomid}`);
-            } else {
-              // Show an error pop-up for other errors.
-              error_pop_up("Error " + userResponse.status);
-            }
-          } catch (error) {
-            // Show an error pop-up for other errors.
-            error_pop_up("Error ");
+            axios
+              .put(`${server}/rooms/${roomid}/users`, { user: userID })
+              .then((response) => {
+                if (response.status === 200) {
+                  // If the user was successfully added to the room, show a success pop-up and navigate to the room.
+                  success_pop_up("You successfully entered the Room");
+                  navigate(`/room/${roomid}`);
+                } else {
+                  // If there was an error adding the user to the room, show an error pop-up.
+                  error_pop_up("Error " + response.status);
+                }
+              })
+              .catch((error) => {
+                // If there was an error with the PUT request, show an error pop-up.
+                error_pop_up("Error " + error.message);
+              });
+          } else {
+            // If there was an error with the POST request to create a room, show an error pop-up.
+            error_pop_up("Error " + response.status);
           }
-        }
-      } else {
-        // Show an error pop-up for server errors.
-        error_pop_up("Error " + response.status);
-      }
-    } catch (error) {
-      // Show an error pop-up for other errors.
-      error_pop_up("Error ");
+        })
+        .catch((error) => {
+          // If there was an error with the POST request, show an error pop-up.
+          error_pop_up("Error " + error.message);
+        });
     }
   };
 
@@ -157,7 +157,8 @@ function Navigation() {
           </Nav>
         </Navbar.Collapse>
       </Container>
-      <ToastContainer /> {/* Render the toast notification container for pop-ups. */}
+      <ToastContainer />{" "}
+      {/* Render the toast notification container for pop-ups. */}
     </Navbar>
   );
 }
